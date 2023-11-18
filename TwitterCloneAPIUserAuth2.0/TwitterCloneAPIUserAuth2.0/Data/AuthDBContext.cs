91b9e97f3e859f1f2b914b80794dacefd787b2b9
BlakeBrewen <brewen.blake4@gmail.com>
2023-11-18 00:05:46 -0600

    the big marge

---
+++ TwitterCloneAPIUserAuth2.0/TwitterCloneAPIUserAuth2.0/Data/AuthDBContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TwitterCloneShared.SharedModels;
+
+namespace TwitterCloneAPIUserAuth2._0.Data
+{
+    public class AuthDbContext : IdentityDbContext<User>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        // Override the OnModelCreating to tweak the model as needed
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Example: If you wanted to rename the ASP.NET Identity table names, you could do it here.
+            // This is optional and can be removed if not needed.
+            modelBuilder.Entity<User>().ToTable("AuthUsers");
+            modelBuilder.Entity<IdentityRole>().ToTable("AuthRoles");
+            modelBuilder.Entity<IdentityUserRole<string>>().ToTable("AuthUserRoles");
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("AuthUserClaims");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("AuthUserLogins");
+            modelBuilder.Entity<IdentityRoleClaim<string>>().ToTable("AuthRoleClaims");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("AuthUserTokens");
+
+            // Add any additional configuration here
+        }
+    }
+}
+
---
+++ TwitterCloneAPIUserAuth2.0/TwitterCloneAPIUserAuth2.0/Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using TwitterCloneAPIUserAuth2._0.Data;
+using TwitterCloneShared.SharedModels;
+
+namespace TwitterCloneAPIUserAuth2._0.Repositories
+{
+    public class UserRepository
+    {
+        private readonly AuthDbContext _context;
+
+        public UserRepository(AuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public User GetById(string userId)
+        {
+            return _context.Users.FirstOrDefault(u => u.Id == userId);
+        }
+
+        public User GetByEmail(string email)
+        {
+            return _context.Users.FirstOrDefault(u => u.Email == email);
+        }
+
+        public User Create(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+
+    }
+}
